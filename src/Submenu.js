0e4d96c7f222055d8e7105c880997f853ba40902
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-03-25 20:38:00 +0100

    first commit

---
+++ src/Submenu.js
@@ -0,0 +1,10 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from "./context";
+
+
+const Submenu = () => {
+  const {isSubmenuOpen} = useGlobalContext()
+  return <aside className={`${isSubmenuOpen} ? 'submenu show' : 'submenu'`}>submenu component</aside>
+}
+
+export default Submenu
